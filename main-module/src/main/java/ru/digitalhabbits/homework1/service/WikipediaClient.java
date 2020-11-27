09249d1d3a79a3f28fd6b5b6a9f914c57efc7fe6
Anton Alputov <alputov@gmail.com>
2020-11-27 22:06:01 +0500

    WikipediaClient. Get wiki api json response

--- main-module/src/main/java/ru/digitalhabbits/homework1/service/WikipediaClient.java
+++ main-module/src/main/java/ru/digitalhabbits/homework1/service/WikipediaClient.java
@@ -1,8 +1,16 @@
 package ru.digitalhabbits.homework1.service;
 
+import com.google.gson.JsonParser;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 
 import javax.annotation.Nonnull;
+import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 
@@ -12,8 +20,15 @@ public class WikipediaClient {
     @Nonnull
     public String search(@Nonnull String searchString) {
         final URI uri = prepareSearchUrl(searchString);
-        // TODO: NotImplemented
-        return "";
+        HttpGet request = new HttpGet(uri);
+        String result = "";
+        try (CloseableHttpClient client = HttpClients.createDefault();
+             CloseableHttpResponse response = client.execute(request)) {
+            HttpEntity entity = response.getEntity();
+            result = getExtractFromJson(EntityUtils.toString(entity));
+        } catch (IOException e) {
+        }
+        return result;
     }
 
     @Nonnull
@@ -30,4 +45,14 @@ public class WikipediaClient {
             throw new RuntimeException(exception);
         }
     }
+
+    private String getExtractFromJson(String jsonString) {
+        return JsonParser.parseString(jsonString)
+                .getAsJsonObject()
+                .getAsJsonObject("query")
+                .getAsJsonObject("pages").entrySet().stream()
+                .findFirst().get()
+                .getValue().getAsJsonObject().get("extract").getAsString();
+    }
+
 }
