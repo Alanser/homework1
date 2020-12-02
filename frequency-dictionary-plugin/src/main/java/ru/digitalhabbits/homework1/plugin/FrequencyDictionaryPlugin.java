46ea5427f6a7f3cc7c69a7322a616d15e769ab5d
Anton Alputov <alputov@gmail.com>
2020-12-02 17:46:27 +0500

    FrequencyDictionaryPlugin

--- frequency-dictionary-plugin/src/main/java/ru/digitalhabbits/homework1/plugin/FrequencyDictionaryPlugin.java
+++ frequency-dictionary-plugin/src/main/java/ru/digitalhabbits/homework1/plugin/FrequencyDictionaryPlugin.java
@@ -2,6 +2,12 @@ package ru.digitalhabbits.homework1.plugin;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.regex.MatchResult;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 public class FrequencyDictionaryPlugin
         implements PluginInterface {
@@ -9,7 +15,16 @@ public class FrequencyDictionaryPlugin
     @Nullable
     @Override
     public String apply(@Nonnull String text) {
-        // TODO: NotImplemented
-        return "FrequencyDictionaryPlugin: NotImplemented";
+        Pattern pattern = Pattern.compile("(\\b[a-zA-Z][a-zA-Z.0-9]*\\b)");
+        Matcher matcher = pattern.matcher(text);
+
+        Map<String, Integer> wordFrequency = new TreeMap<>();
+        Stream<MatchResult> results = matcher.results();
+        results.forEach(mr -> wordFrequency.merge(mr.group().toLowerCase(), 1, Integer::sum));
+        String result = wordFrequency.entrySet().stream()
+                .map(entry -> entry.getKey() + " " + entry.getValue() + "\n")
+                .reduce("", String::concat);
+
+        return result;
     }
 }
