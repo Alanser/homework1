d526e0bdecca6c7fb25debda36fc43ff88f435a0
Anton Alputov <alputov@gmail.com>
2020-12-04 13:34:33 +0500

    PluginEngine

--- main-module/src/main/java/ru/digitalhabbits/homework1/service/PluginEngine.java
+++ main-module/src/main/java/ru/digitalhabbits/homework1/service/PluginEngine.java
@@ -3,12 +3,20 @@ package ru.digitalhabbits.homework1.service;
 import ru.digitalhabbits.homework1.plugin.PluginInterface;
 
 import javax.annotation.Nonnull;
+import java.lang.reflect.Method;
 
 public class PluginEngine {
 
     @Nonnull
-    public  <T extends PluginInterface> String applyPlugin(@Nonnull Class<T> cls, @Nonnull String text) {
-        // TODO: NotImplemented
-        return "NotImplemented";
+    public <T extends PluginInterface> String applyPlugin(@Nonnull Class<T> cls, @Nonnull String text) {
+        String result = "";
+        try {
+            T plugin = cls.getDeclaredConstructor().newInstance();
+            Method apply = PluginInterface.class.getDeclaredMethod("apply", String.class);
+            result = (String) apply.invoke(plugin, text);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
     }
 }
